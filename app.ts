c6e9256eee1eb306a327fa05d366890a5a4a9abf
beark93 <beark93@gmail.com>
2023-10-25 22:14:13 +0900

    modify api url

--- app.ts
+++ app.ts
@@ -5,7 +5,7 @@ import router from './routes';
 const app = express();
 const port = 3001;
 
-app.use('/v1', router);
+app.use('/api/v1', router);
 
 app.listen(port, () => {
   console.log(`Timezones by location application is running on port ${port}.`);
